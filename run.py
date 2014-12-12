911aab5c79fd3a9101d07f314d82019b28386f1b
Carolyn Saund <ca.m.saund@gmail.com>
2014-12-12 17:27:38 -0500

    making main generation loop. Really leaving now.

--- run.py
+++ run.py
@@ -1,14 +1,36 @@
 #main stuff
 
 import fieldbots as fb
+import genetics as gen
+
+BATCH_SIZE = 100 	#arbitrary batch size.
+GENERATIONS = 100 #arbitrary generations. This should converge, I hope.
+
 
 if __name__ == "__main__":
-	field = fb.Field(10,10)
+	#starting population
+	population = []
+	field = fb.Field()
+	for bot in range(BATCH_SIZE):
+		b = fb.WaterBot()
+		b.random_path()
+		b.execute_path(field)
+		population.append((b, field.fitness()))
 
-	waterbot = fb.WaterBot()
-	waterbot.random_path()
-	print waterbot.path
+	for gen in range(GENERATIONS):
+		#sort population by fitness
+		population.sort(key=lambda x: x[1])
+		#keep best half
+		population = population[50:]
 
-	waterbot.execute_path(field)
-	field.print_field()
-	print field.fitness()
+		#randomly segment into 3 lists
+			#crossover
+			#mutation
+			#leave the same
+
+		#leave most fit 50 in the population?
+
+
+
+
+
